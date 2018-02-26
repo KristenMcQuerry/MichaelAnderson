840614aaac87630a92d1630bfc1d4cca7da9741f
mdan227 <michael.anderson@uky.edu>
2018-02-26 15:44:25 -0500

    answered all the questions

--- 693 Spring 2018 Assignment 1.R
+++ 693 Spring 2018 Assignment 1.R
@@ -1,6 +1,6 @@
 ##install packages
-install.packages("tidyverse")
-install.packages("nycflights13")
+#install.packages("tidyverse")
+#install.packages("nycflights13")
 
 ## load libraries
 library(tidyverse)
@@ -8,3 +8,49 @@ library(nycflights13)
 
 ## dataset for assignment
 flights
+
+# Part a
+flights %>% 
+  filter(arr_delay >= 120)
+
+# Part b
+flights %>% 
+  filter(dest %in% c('IAH','HOU'))
+
+# Part c
+flights %>% 
+  filter(carrier %in% c('UA','AA','DL'))
+
+# Part d
+flights %>% 
+  filter(month %in% c(7,8,9))
+
+# Part e
+flights %>% 
+  filter(arr_delay >= 120 & dep_delay <= 0)
+
+# Part f
+flights %>% 
+  filter(dep_delay >= 60 & arr_delay < dep_delay-30)
+
+# Part g
+flights %>% 
+  filter(dep_time <= 600)
+
+# Part h
+flights %>% 
+  arrange(desc(dep_delay))
+
+flights %>% 
+  arrange(dep_delay)
+
+# Part i
+flights %>% 
+  arrange(desc(distance/air_time))
+
+# Part j
+flights %>% 
+  arrange(desc(distance))
+
+flights %>% 
+  arrange(distance)
